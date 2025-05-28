db2a150cadef85dbb3c8086bdabf996e2aa81598
Your Name <federickgrendalo@gmail.com>
2025-05-28 21:27:22 +0300

    infoCards design

--- components/UI/Homepage/HowItWorks.jsx
+++ components/UI/Homepage/HowItWorks.jsx
@@ -1,8 +1,57 @@
-import { Check, CircleCheck, Shield } from "lucide-react";
+"use client";
+import { Check, CircleCheck, Download, GitGraph, Info, Shield, Tag } from "lucide-react";
+import { motion } from "framer-motion";
+import InfoCard from "./InfoCard";
+const steps = [
+  {
+    title: "Prisijunk prie savo banko",
+    description: "Susiek savo banko sąskaitą su GoCardless - Europos lyderio mokėjimų srityje.",
+    icon: "Shield",
+    list: [
+      "Saugus ir patikimas prisijungimas",
+      "PSD2 sertifikuotas",
+      "Greitas ir paprastas procesas",
+    ],
+  }, 
+  {
+    title: "Automatinis operacijų importas",
+    description: "Visi tavo mokėjimai bus automatiškai importuojami į mūsų sistemą.",
+    icon: "Download",
+    list: [
+      "Duomenų sinchronizavimas realiu laiku",
+    ]
+  },
+  {
+    title: "Išmanus kategorizavimas",
+    description: "Susikurk kategorijas ir priskirk prie jų visas savo banko operacijas.",
+    icon: "Tag",
+    list: [
+      "Individualios kategorijos",
+      "Galimybė išskaidyti operacijas į kelias kategorijas"
+    ]
+  },
+  {
+    title: "Mėnesio analizė",
+    description: "Mėnesio pabaigoje gauk išsamią skaitinę ir grafinę pinigų srauto analizę su DI įžvalgomis.",
+    icon: "GitGraph",
+    list: [
+      "Grafinė analizė",
+      "Išlaidų tendencijos",
+      "DI įžvalgos"
+    ]
+  }
+]
 export default function HowItWorks() {
   return (
-    <section className="w-full text-white flex-col flex h-auto items-center justify-center">
-      <h2 className=" text-center font-bold sm:text-4xl text-2xl">Kaip viskas veikia?</h2>
+    <motion.section
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6, ease: "easeOut" }}
+      className="w-full text-white flex-col flex h-auto items-center justify-center"
+    >
+      <h2 className=" text-center font-bold sm:text-4xl text-2xl">
+        Kaip viskas veikia?
+      </h2>
       <div className="flex flex-col justify-center gap-y-2">
         <p className="text-gray-400 text-center">
           Štai kaip automatizuojam visus tavo finansus.
@@ -13,7 +62,7 @@ export default function HowItWorks() {
             <span>GoCardless</span>
           </div>
           <div className="flex items-center gap-1">
-            <Shield size={32} stroke="var(--color-secondary)"/>
+            <Shield size={32} stroke="var(--color-secondary)" />
             <span>Saugus prijungimas prie banko</span>
           </div>
           <div className="sm:flex items-center gap-1 hidden">
@@ -22,6 +71,13 @@ export default function HowItWorks() {
           </div>
         </div>
       </div>
-    </section>
+
+      {/** Steps */}
+      <div>
+        {steps.map((step, index) => (
+          <InfoCard {...step} id={index} key={step.title}/>
+        ))}
+      </div>
+    </motion.section>
   );
 }
--- components/UI/Homepage/InfoCard.jsx
+++ components/UI/Homepage/InfoCard.jsx
@@ -1,7 +1,42 @@
-export default function InfoCard({ title, description, icon, list, id}) {
-    return (
-        <div>
-            
-        </div>
-    )
+import {
+  Check,
+  CheckCircle,
+  Download,
+  GitGraph,
+  Info,
+  Shield,
+  Tag,
+} from "lucide-react";
+
+const Icons = {
+  Shield: Shield,
+  Download: Download,
+  GitGraph: GitGraph,
+  Tag: Tag,
+};
+
+export default function InfoCard({ title, description, icon, list, id }) {
+  const IconComponent = Icons[icon];
+
+  return (
+    <div className="bg-gray-900 p-4 rounded-lg text-white space-y-2 max-w-80">
+      <span className="w-6 h-6 rounded-full bg-gray-700 flex items-center justify-center text-xs font-bold">
+        {id + 1}
+      </span>
+      {IconComponent && <IconComponent size={32} />}
+      <h4 className="text-lg font-semibold">{title}</h4>
+      <p className="text-sm text-gray-300">{description}</p>
+      <ul>
+        {list.map((item, index) => (
+          <li className="flex items-center text-sm text-gray-200" key={index}>
+            <CheckCircle
+              className="inline-block mr-2 text-green-500"
+              size={16}
+            />
+            <span>{item}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
